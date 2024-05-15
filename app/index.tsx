6e53dbdc82e96d55f9cb4e207b327754c26b189c
Wahome <vinwahome@gmail.com>
2024-05-15 10:25:58 +0300

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import Login from './Screens/LoginScreen/Login'
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Login />
+    </View>
+  );
+}
