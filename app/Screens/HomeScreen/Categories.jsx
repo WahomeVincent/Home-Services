0eacaa6db719d4915242090ae588d1d07b506de3
Wahome <vinwahome@gmail.com>
2024-05-26 12:26:42 +0300

    Fetch and display the categories section

---
+++ app/Screens/HomeScreen/Categories.jsx
@@ -0,0 +1,62 @@
+import { View, Text, FlatList, Image, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import Heading from '../../Components/Heading'
+import GlobalApi from '../../utils/GlobalApi'
+import Colors from '@/app/utils/Colors'
+
+
+export default function Categories() {
+
+    const [category, setCategory] = useState([])
+    useEffect(()=> {
+        getCategories()
+    }, [])
+    const getCategories = () => {
+        GlobalApi.getCategory().then(resp => {
+        //   console.log('res', resp?.categories);
+        setCategory(resp.categories)
+        })
+    }
+  
+  return (
+    <View>
+        <View style={styles.heading}>
+            <Heading text='Categories'/>
+            <Text style={{marginTop:10, color:'#6b6b47'}}>View All</Text>
+        </View>
+        <FlatList 
+            data={category}
+            numColumns={4}
+            renderItem={({item,index}) =>  (
+                <View style={styles.container}>
+                    <View style={styles.iconContainer}>
+                        <Image source={{uri:item.icon.url}} style={{width:30, height:30}}/>
+                    </View>
+                    <Text style={{fontWeight:'bold', marginTop:5}}>{item.name}</Text>
+                </View>
+            )}
+        />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    heading:{
+        display:'flex',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between'
+    },
+    container:{
+        flex:1,
+        alignItems:'center',
+        marginTop:10
+    },
+    iconContainer:{
+        flex:1,
+        padding:17,
+        backgroundColor:Colors.WHITE,
+        borderRadius:99
+      },
+
+})
--- app/Screens/HomeScreen/HomeScreen.jsx
+++ app/Screens/HomeScreen/HomeScreen.jsx
@@ -2,12 +2,18 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Header from './Header'
 import Colors from '../../utils/Colors'
+import Slider from './Slider'
+import Categories from './Categories'
 
 
 export default function HomeScreen() {
   return (
-    <View style={{backgroundColor:Colors.BACKGROUND}}>
+    <View style={{backgroundColor:Colors.BACKGROUND, padding:10}}>
       <Header />
+      <View style={{padding:10}}>
+        <Slider />
+        <Categories />
+      </View>
     </View>
   )
 }
