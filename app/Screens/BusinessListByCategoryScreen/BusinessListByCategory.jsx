99bd1424152273f1890730da7ca53f97ac489764
Wahome <vinwahome@gmail.com>
2024-05-30 09:25:49 +0300

    Navigation to go back

--- app/Screens/BusinessListByCategoryScreen/BusinessListByCategory.jsx
+++ app/Screens/BusinessListByCategoryScreen/BusinessListByCategory.jsx
@@ -1,16 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useEffect } from 'react'
 import { useRoute } from '@react-navigation/native'
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { useNavigation } from '@react-navigation/native';
+
+
 
 export default function BusinessListByCategory() {
   const param = useRoute().params;
+  const navigation = useNavigation();
+
 
   useEffect(() => {
-      console.log(param);
+      console.log(param.category);
   }, [])
   return (
     <View>
-      <Text>BusinessListByCategory</Text>
+      <TouchableOpacity onPress={() => navigation.goBack()} style={{padding:20, display:'flex', flexDirection:'row', gap:10, alignItems:'center'}}>
+        <FontAwesome5 name="arrow-left" size={24} color="black" />
+        <Text style={{fontSize:22}}>{param.category}</Text>
+      </TouchableOpacity>
     </View>
   )
 }
