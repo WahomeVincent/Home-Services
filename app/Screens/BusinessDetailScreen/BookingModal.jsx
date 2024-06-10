8981a2f2060bdfa4d3a52173ccda300c5bdc02fe
Wahome <vinwahome@gmail.com>
2024-06-10 21:03:41 +0300

    Style the calender

--- app/Screens/BusinessDetailScreen/BookingModal.jsx
+++ app/Screens/BusinessDetailScreen/BookingModal.jsx
@@ -1,19 +1,53 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5'
 import CalendarPicker from "react-native-calendar-picker";
+import Colors from '../../utils/Colors'
+import Heading from '@/app/Components/Heading';
 
 
 
 export default function BookingModal({hideModal}) {
+
+  // useEffect(() => {
+  //   getTime()
+  // }, [])
+  
+
+  // const getTime = () => {
+  //     const timeList = [];
+  //     for (let i = 8; i <= 12; i++) {
+  //       timeList.push({
+  //         time:i + ':00AM'
+  //       })
+        
+  //     }
+  // }
   return (
     <View>
-      <TouchableOpacity  style={{position:'absolute', top:10, left:20}} onPress={() => hideModal()} >
+      <TouchableOpacity onPress={() => hideModal()} style={{marginHorizontal:20, marginTop:20}} >
           <FontAwesome5 name="arrow-left" size={28} color="black" />
       </TouchableOpacity>
-      <View style={{marginVertical:40, padding:20}}>
-          <CalendarPicker />
+      <View style={{marginHorizontal:20, marginBottom:10}}>
+        <Heading text={'Select Date'} />
+      </View>
+      <View style={styles.calendar}>
+          <CalendarPicker onDateChange={this.onDateChange}
+              width={350}
+              minDate={Date.now()}
+              todayBackgroundColor={Colors.WHITE}
+              selectedDayColor={Colors.GRAY}
+          />
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  calendar:{
+    backgroundColor:Colors.PRIMARY,
+    marginHorizontal:20,
+    borderRadius:10,
+    padding:10
+  }
+})
